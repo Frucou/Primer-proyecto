6cbe52b56f9ef36c6780695fb1f620f98e2ca207
fabian <fruicou.sansus3@gmail.com>
2021-11-02 09:39:53 +0100

    Añadido el js con ejercicios
    -Creación del js
    -Ejercicios para los alumnos hecho
    -Ejercicio para resolver con el profe hechos hasta el punto 2

---
+++ js/scripts.js
@@ -0,0 +1,38 @@
+"use strict";
+// la linea anterior "siempre"
+
+// Ejercicios para los alumnos
+// let nombre = "Fabian";
+
+// console.log(nombre);
+
+// nombre = "David";
+
+// console.log(nombre);
+// la siguiente línea declara varias variables en una sola línea
+// let operador1, operador2 = 8;
+let operador1 = 8;
+let operador2 = 8;
+let resultado;
+
+resultado = operador1 + operador2;
+console.log(resultado);
+
+// Ejercicios con el profe
+// area de un círculo
+let radio = 2;
+const PI = 3.1416;
+let area;
+
+area = PI * Math.pow(radio, 2);
+console.log(area);
+
+// calcular el área de un triangulo
+let b = 4;
+let h = 5;
+let areaTriagulo;
+
+areaTriagulo = b * h / 2;
+console.log(areaTriagulo);
+
+// calcular la altura
